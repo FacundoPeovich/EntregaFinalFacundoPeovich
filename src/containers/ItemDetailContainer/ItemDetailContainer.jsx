e48e3f489f1c26dee3c4b08345663b9ad5e4b4bb
Facundo Peovich <peovichfacundo@gmail.com>
2023-10-24 20:14:55 -0300

    Entrega n2 correcion ItemDetail3

--- src/containers/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/containers/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom"
 import { useEffect, useState } from "react"
 import Spinner from 'react-bootstrap/Spinner'
-import "./ItemDetailContainer.css"
+import "./itemDetailContainer.css"
 
 
 
