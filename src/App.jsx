06bc175041ff85af56c99992a706ecdfc9a546c0
Facundo Peovich <peovichfacundo@gmail.com>
2023-10-24 18:22:06 -0300

    Entrega n2 etapa 2: Footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import NotFound from './components/NotFound/NotFound'
 import ItemDetailContainer from './containers/ItemDetailContainer/ItemDetailContainer'
+import Footer from './components/Footer/Footer'
 
 
 
@@ -20,14 +21,10 @@ function App() {
           <Route exact path='/' element={<ItemListContainer greeting="Hola, bienvenido a mi página" />} />
           <Route exact path="/category/:nombreCategoria" element={<ItemListContainer />} />
           <Route path="/item/:id" element={<ItemDetailContainer />} />
-          
           <Route path='*' element={<NotFound />} />
         </Routes>
-
+        <Footer />
       </BrowserRouter>
-
-
-
     </>
   )
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <div className="footer-content">
+                <p>&copy; 2023 FP Electrónica</p>
+                <p>Contacto: info@fpelectronica.com.ar</p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
